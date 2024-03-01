4bcca531268d4f6b6c460ab517408f831b188733
Jouzer <32982491+Jouzer@users.noreply.github.com>
2024-03-01 21:51:59 +0200

    Try out the secondary addressing
    You need to set the secondary address inside the .py for this test

---
+++ examples/mbus-test-secondary.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# ------------------------------------------------------------------------------
+# Copyright (C) 2012, Robert Johansson <rob@raditex.nu>, Raditex Control AB
+# All rights reserved.
+# ------------------------------------------------------------------------------
+
+"""
+mbus test: send a request frame and receive and parse the reply
+"""
+
+from mbus.MBus import MBus
+
+debug = True
+#address = 0
+address = 0xFD #secondary
+ret = mbus.select_secondary_address("24134746FFFFFFFF")
+
+
+#mbus = MBus(host="mbus-gw1", port=8888)
+mbus = MBus(device="/dev/ttyUSB0", libpath="/home/ph/git/python-mbus/examples/libmbus.so")
+
+if debug:
+    print("mbus = " + str(mbus))
+
+mbus.connect()
+
+if debug:
+    print("mbus = " + str(mbus))
+
+mbus.serial_set_baudrate(2400)
+
+#res = mbus.send_ping_frame(0xFD, 1)
+#print(res)
+#res = mbus.send_ping_frame(0, 1)
+#print(res)
+# fairly sure these pings not needed, try
+
+mbus.send_request_frame(address)
+
+reply = mbus.recv_frame()
+
+if debug:
+    print("reply =", reply)
+
+reply_data = mbus.frame_data_parse(reply)
+
+if debug:
+    print("reply_data =", reply_data)
+
+xml_buff = mbus.frame_data_xml(reply_data)
+
+print("xml_buff =", xml_buff)
+
+mbus.frame_data_free(reply_data)
+
+mbus.disconnect()
